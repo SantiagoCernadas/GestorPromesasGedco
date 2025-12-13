deb767630268a957419a609ff28833e0e8b958f1
santiagoCernadas <santiagocernadas29@gmail.com>
2025-12-13 18:08:40 -0300

    se implementan los test de promesaService

--- Backend/src/test/java/com/crmpps/Backend/service/PromesaServiceTest.java
+++ Backend/src/test/java/com/crmpps/Backend/service/PromesaServiceTest.java
@@ -1,10 +1,12 @@
 package com.crmpps.Backend.service;
 
+import com.crmpps.Backend.dto.PromesaExcelRequest;
 import com.crmpps.Backend.dto.PromesaRequest;
 import com.crmpps.Backend.dto.enums.Canales;
 import com.crmpps.Backend.dto.enums.EstadosCumplimiento;
 import com.crmpps.Backend.dto.enums.Sites;
 import com.crmpps.Backend.dto.enums.TiposAcuerdo;
+import com.crmpps.Backend.entity.PromesaEntity;
 import com.crmpps.Backend.entity.Rol;
 import com.crmpps.Backend.entity.UsuarioEntity;
 import com.crmpps.Backend.exception.LogicaInvalidaException;
@@ -22,10 +24,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -50,6 +49,8 @@ class PromesaServiceTest {
 
     private PromesaRequest promesaRequest;
     private UsuarioEntity usuarioEntity;
+    private PromesaEntity promesaEntity;
+    private List<PromesaExcelRequest> listaPromesas;
     private Map<String,String> headers;
 
     @BeforeEach
@@ -69,17 +70,49 @@ class PromesaServiceTest {
                 .operador(1L)
                 .build();
 
+
         usuarioEntity = UsuarioEntity
                 .builder()
                 .id(1L)
-                .nombreUsuario("Usuario_prueba")
-                .nombre("Nombre_prueba")
+                .nombreUsuario("Usuario_test")
+                .nombre("Nombre_test")
                 .rol(Rol.OPERADOR)
-                .contrasenia("prueba")
+                .contrasenia("test")
                 .build();
 
+        promesaEntity = PromesaEntity
+                .builder()
+                .id(1L)
+                .idUsuarioML(2314L)
+                .numCaso(3425L)
+                .monto(200.20)
+                .site(Sites.MLA.getSite())
+                .canal(Canales.CHAT.getCanal())
+                .tipoAcuerdo(TiposAcuerdo.PAGO_PARCIAL.getTipoAcuerdo())
+                .cumplimiento(EstadosCumplimiento.EN_CURSO.getEstado())
+                .fechaCarga(LocalDate.now())
+                .fechaPago(LocalDate.now())
+                .operador(usuarioEntity)
+                .build();
+
+
+        PromesaExcelRequest promesaExcelRequest = PromesaExcelRequest.builder()
+                .idUsuarioML(2314L)
+                .numCaso(3425L)
+                .monto(200.20)
+                .site(Sites.MLA.getSite())
+                .canal(Canales.CHAT.getCanal())
+                .tipoAcuerdo(TiposAcuerdo.PAGO_PARCIAL.getTipoAcuerdo())
+                .cumplimiento(EstadosCumplimiento.EN_CURSO.getEstado())
+                .fechaCarga(LocalDate.now())
+                .fechaPago(LocalDate.now())
+                .operador("Test")
+                .build();
+
+        listaPromesas = Arrays.asList(promesaExcelRequest);
+
         headers = new HashMap<>();
-        headers.put("authorization","Bearer pruebadetoken");
+        headers.put("authorization","Bearer testToken");
     }
     @Test
     void agregarPromesa_rolOperador_Ok() {
@@ -224,26 +257,205 @@ class PromesaServiceTest {
     }
 
     @Test
-    void obtenerPromesa() {
+    void obtenerPromesa_rolOperador_Ok() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_OPERADOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario());
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertDoesNotThrow(() -> {
+            promesaService.obtenerPromesa(headers,any(Long.class));
+        });
     }
 
     @Test
-    void eliminarPromesa() {
+    void obtenerPromesa_rolOperador_CredencialesInvalidas() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_OPERADOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario()+"diferencia");
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertThrows(NoAutorizadoException.class ,() -> {
+            promesaService.obtenerPromesa(headers,any(Long.class));
+        });
     }
 
     @Test
-    void modificarPromesa() {
+    void obtenerPromesa_rolSupervisor_Ok() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_SUPERVISOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario()+"diferencia");
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertDoesNotThrow(() -> {
+            promesaService.obtenerPromesa(headers,any(Long.class));
+        });
     }
 
     @Test
-    void getEstadisticas() {
+    void eliminarPromesa_rolOperador_Ok() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_OPERADOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario());
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertDoesNotThrow(() -> {
+            promesaService.eliminarPromesa(headers,any(Long.class));
+        });
     }
 
     @Test
-    void promesaDuplica() {
+    void eliminarPromesa_rolOperador_CredencialesInvalidas() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_OPERADOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario()+"diferencia");
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertThrows(NoAutorizadoException.class ,() -> {
+            promesaService.eliminarPromesa(headers,any(Long.class));
+        });
     }
 
     @Test
-    void getExcelTabla() {
+    void eliminarPromesa_rolSupervisor_Ok() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_SUPERVISOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario()+"diferencia");
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertDoesNotThrow(() -> {
+            promesaService.eliminarPromesa(headers,any(Long.class));
+        });
+    }
+
+    @Test
+    void modificarPromesa_rolOperador_Ok() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_OPERADOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario());
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertDoesNotThrow(() -> {
+            promesaService.modificarPromesa(headers,any(Long.class),promesaRequest);
+        });
+    }
+
+    @Test
+    void modificarPromesa_rolOperador_CredencialesInvalidas() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_OPERADOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario()+"diferencia");
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertThrows(NoAutorizadoException.class ,() -> {
+            promesaService.modificarPromesa(headers,any(Long.class),promesaRequest);
+        });
+    }
+
+    @Test
+    void modificarPromesa_rolSupervisor_Ok() {
+        when(usuarioRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(usuarioEntity));
+
+        when(jwtUtils.getRolFromToken(any(String.class)))
+                .thenReturn("ROLE_SUPERVISOR");
+
+        when(jwtUtils.getNombreUsuarioFromToken(any(String.class)))
+                .thenReturn(usuarioEntity.getNombreUsuario()+"diferencia");
+
+        when(promesaRepository.findById(any(Long.class)))
+                .thenReturn(Optional.of(promesaEntity));
+
+        assertDoesNotThrow(() -> {
+            promesaService.modificarPromesa(headers,any(Long.class),promesaRequest);
+        });
+    }
+
+    @Test
+    void getEstadisticas_Ok() {
+        assertDoesNotThrow(() -> {
+            promesaService.getEstadisticas(headers,listaPromesas);
+        });
+    }
+
+    @Test
+    void getEstadisticas_ListaVacia_LogicaInvalida() {
+        listaPromesas = new ArrayList<>();
+
+        assertThrows(LogicaInvalidaException.class,() -> {
+            promesaService.getEstadisticas(headers,listaPromesas);
+        });
+    }
+
+    @Test
+    void getExcelTabla_Ok() {
+        assertDoesNotThrow(() -> {
+            promesaService.getExcelTabla(headers,listaPromesas);
+        });
+    }
+
+    @Test
+    void getExcelTabla_ListaVacia_LogicaInvalida() {
+        listaPromesas = new ArrayList<>();
+
+        assertThrows(LogicaInvalidaException.class,() -> {
+            promesaService.getExcelTabla(headers,listaPromesas);
+        });
     }
 }
