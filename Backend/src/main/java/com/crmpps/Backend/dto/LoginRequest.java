692f55b4deae233d24ab652453ede830be8264eb
santiagoCernadas <santiagocernadas29@gmail.com>
2025-12-13 20:35:26 -0300

    se implementan los test para authservice

--- Backend/src/main/java/com/crmpps/Backend/dto/LoginRequest.java
+++ Backend/src/main/java/com/crmpps/Backend/dto/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.crmpps.Backend.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class LoginRequest {
     private String nombreUsuario;
     private String contrasenia;
