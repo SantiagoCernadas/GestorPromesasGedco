345ccb6c74622674993b474a8b0aacb731671d10
santiagoCernadas <santiagocernadas29@gmail.com>
2026-01-02 20:07:13 -0300

    se agrega link correcto al cors

--- Backend/src/main/java/com/crmpps/Backend/config/WebMvcConfig.java
+++ Backend/src/main/java/com/crmpps/Backend/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500","http://localhost:5500","https://santiagocernadas.github.io/GestorPromesasGedco")
+                .allowedOrigins("http://127.0.0.1:5500","http://localhost:5500","https://santiagocernadas.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
